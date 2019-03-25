7680c8cfcafab47859f1c2ca339a210b2ee5a428
lihaiyang2046 <1160362008@qq.com>
2019-03-25 13:59:39 +0800

    jbf-spring-cloud完成eurake服务和zull服务

---
+++ jbf-zull/src/main/java/com/jbf/spring/jbfzull/JbfZullApplication.java
@@ -0,0 +1,17 @@
+package com.jbf.spring.jbfzull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@EnableEurekaClient
+@SpringBootApplication
+public class JbfZullApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JbfZullApplication.class, args);
+    }
+
+}
